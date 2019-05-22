86318aa7d56176d81ed01da45da4d73feab0a16d
Djamilson <djamilson@gmail.com>
2019-05-22 09:32:19 -0300

    feat: Deploy Final AWS

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const cors = require("cors");
+
+const PORT = 3000;
+const HOST = '0.0.0.0';
 
 const app = express();
+app.use(cors());
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(3000);
+app.get("/", (req, res) => res.send("Hello World Na Aws!"));
+app.listen(PORT, HOST);
